c2bc672fa0403c0ed64760066679c2ce50633902
joeyb04 <sjoeyb04@csu.fullerton.edu>
2026-02-18 03:54:42 -0800

    Added node implementation

---
+++ Huffman.h
@@ -0,0 +1,15 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+struct Node {
+    char ch;
+    int freq;
+    Node *left, *right;
+    
+    Node(char character, int frequency, Node* l = nullptr, Node* r = nullptr)
+        : ch(character), freq(frequency), left(l), right(r) {}
+};
